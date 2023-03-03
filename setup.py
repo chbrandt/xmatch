52145e493005d4db12792d9091dbe6b350adc406
Carlos H Brandt <c.brandt@jacobs-university.de>
2023-03-03 10:28:01 +0100

    Move setup attributes to cfg file

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@ import versioneer
 
 scripts = ['bin/xmatch', 'bin/_xmatch.py']
 
-setup(name='xmatch',
-      description='Astronimcal catalogs cross-matching tool',
-      packages=['xmatch'],
+setup(
       scripts=scripts,
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
-      url='http://github.com/chbrandt/xmatch'
-      )
+)
